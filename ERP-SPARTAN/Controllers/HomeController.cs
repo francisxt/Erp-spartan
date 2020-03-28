bfde101f33c339f0d2b21d1c77baa513b3e29d19
orbis25 <orbisalonzo@gmail.com>
2020-03-27 21:13:11 -0400

    adding details in home

--- ERP-SPARTAN/Controllers/HomeController.cs
+++ ERP-SPARTAN/Controllers/HomeController.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using BusinesLogic.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using BusinesLogic.Interfaces;
-using BusinesLogic.UnitOfWork;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Models.Enums;
 
 namespace ERP_SPARTAN.Controllers
 {
-    [Authorize(Roles = nameof(RolsAuthorization.Admin) + "," + nameof(RolsAuthorization.ClientsUser)+ "," + nameof(RolsAuthorization.Client))]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _service;
         public HomeController(IUnitOfWork service) => _service = service;
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var user = User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
-            return View(_service.HomeService.Get(user));
+            return View(await _service.HomeService.Get(user));
         }
 
     }
